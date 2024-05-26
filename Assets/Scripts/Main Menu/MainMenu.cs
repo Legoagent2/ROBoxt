27c8961b094b4c2f203313340e08794379d813b2
HammerAll888 <andrew.coppins1@gmail.com>
2024-05-26 20:59:37 +1000

    Finished the Menu's
    Start sends you to level one. If you hit the levels button itll send you to a screen with 6 level buttons which will send you to the corresponding level and a back button which sends you back to the main menu

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -10,6 +10,51 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadSceneAsync(1);
     }
 
+    public void Options()
+    {
+        SceneManager.LoadSceneAsync(7);
+    }
+
+    public void Levels()
+    {
+        SceneManager.LoadSceneAsync(8);
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+
+    public void Level6()
+    {
+        SceneManager.LoadSceneAsync(6);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
